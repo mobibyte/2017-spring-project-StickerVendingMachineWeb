7db7403bc7888c3c15793b19f23a9d2bab3bf3a5
Nhi Luong <nhi.luong@mavs.uta.edugit config --global user.email nhi.luong@mavs.uta.edugit config --global user.email nhi.luong@mavs.uta.edu>
2017-03-05 01:11:24 -0600

    commit to be safe

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,17 +1,45 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
+import {Button, ButtonToolbar, ControlLabel, ButtonGroup} from "react-bootstrap"
+
 
 class Home extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      quantity: 0
+    };
+  }
+  
+  increment(){
+    const newQuantity = this.state.quantity + 1;
+   this.setState( { quantity: newQuantity});
+  }
+  decrement(){
+    var newQuantity = 0
+    const {quantity} = this.state;
+    // if (this.state.quantity >0){
+    //   const newQuantity = this.state.quantity -1}
+    if (quantity >0){
+      newQuantity = quantity - 1}
+   this.setState( { quantity: newQuantity});
+  }
+  
   render() {
+   
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+        <ButtonGroup >
+          <img style={{width: 50, height: 50}} src = "https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-alpaca.jpg"/>
+           <Button onClick={this.decrement.bind(this)}>-</Button>
+           <div>{this.state.quantity}</div>
+           <Button onClick={this.increment.bind(this)}>+</Button>
+        </ButtonGroup>
+        
+        <ButtonToolbar>
+          <Button bsStyle="primary" bsSize="large">Check Out</Button>
+
+        </ButtonToolbar>
       </div>
     );
   }
