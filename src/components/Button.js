a8fe99224a9f661574e91c1323c2012c528d1e56
Kolten Sturgill <koltensturgill92@gmail.com>
2017-03-04 20:56:12 -0600

    saving is hard

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ import React, {Component} from "react"
 class Button extends Component{
   render(){
     return(
-      <div>
+      <div className="">
         make your button here.
       </div>
     )
