5a0ba12c4a3f1c9e16758eb5ead5cdd19188f8db
Nhi Luong <nhi.luong@mavs.uta.edugit config --global user.email nhi.luong@mavs.uta.edugit config --global user.email nhi.luong@mavs.uta.edu>
2017-03-04 22:24:23 -0600

    koltens an idiot

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
+import CustomButton from '../components/CustomButton';
+import {Button, ButtonToolbar, FormGroup, ControlLabel} from "react-bootstrap"
+
 
 class Home extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Test</h1>
+        <Button bsStyle="primary">Fuck</Button>
       </div>
     );
   }
