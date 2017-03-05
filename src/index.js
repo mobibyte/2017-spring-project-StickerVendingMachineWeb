0fb10898dfec90e886a313c3e7aabd2017f2dea5
Kolten Sturgill <koltensturgill92@gmail.com>
2017-03-05 00:44:12 -0600

    for now

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import Home from './containers/Home';
 import TrackOrder from './containers/TrackOrder';
 
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(
   <Router history={browserHistory}>
