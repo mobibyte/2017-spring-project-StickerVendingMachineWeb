b9a568a938c4c3f698f2fd2a47ac8f3e9fbbf1c6
Nhi Luong <nhi.luong@mavs.uta.edugit config --global user.email nhi.luong@mavs.uta.edugit config --global user.email nhi.luong@mavs.uta.edu>
2017-03-05 00:47:56 -0600

    Added counter

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,14 +1,44 @@
 import React, { Component } from 'react';
-import CustomButton from '../components/CustomButton';
-import {Button, ButtonToolbar, FormGroup, ControlLabel} from "react-bootstrap"
+import {Button, ButtonToolbar, ControlLabel, ButtonGroup} from "react-bootstrap"
 
 
 class Home extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      quantity: 0
+    };
+  }
+  increment(){
+    const newQuantity = this.state.quantity + 1;
+   this.setState( { quantity: newQuantity});
+  }
+  decrement(){
+    var newQuantity = 0
+    const {quantity} = this.state;
+    // if (this.state.quantity >0){
+    //   const newQuantity = this.state.quantity -1}
+    if (quantity >0){
+      newQuantity = quantity - 1}
+   this.setState( { quantity: newQuantity});
+  }
+  
   render() {
+   
     return (
       <div className="App">
-        <h1>Test</h1>
-        <Button bsStyle="primary">Fuck</Button>
+        
+        <ButtonGroup >
+          <img style={{width: 50, height: 50}} src = "https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-alpaca.jpg"/>
+           <Button onClick={this.decrement.bind(this)}>-</Button>
+           <div>{this.state.quantity}</div>
+           <Button onClick={this.increment.bind(this)}>+</Button>
+        </ButtonGroup>
+        
+        <ButtonToolbar>
+          <Button bsStyle="primary" bsSize="large">Check Out</Button>
+
+        </ButtonToolbar>
       </div>
     );
   }
