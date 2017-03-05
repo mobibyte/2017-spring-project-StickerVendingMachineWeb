976335760839a7d9b2405b0fff9873445dfeb80a
Kolten Sturgill <koltensturgill92@gmail.com>
2017-03-04 20:51:16 -0600

    added button template, react-bootstrap

---
+++ src/components/Button.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react"
+
+class Button extends Component{
+  render(){
+    return(
+      <div>
+        make your button here.
+      </div>
+    )
+  }
+}
+
+export default Button;
