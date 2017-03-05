f4e29e7c1d653608382727751bbbb3129f3f985a
Cameron Moreau <cameron.cm6@gmail.com>
2017-03-04 20:30:29 -0600

    Setup react app

---
+++ src/containers/TrackOrder.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TrackOrder extends Component {
+  render() {
+    return (
+      <div>
+        order page
+      </div>
+    );
+  }
+}
+
+export default TrackOrder;
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
+
+import Home from './containers/Home';
+import TrackOrder from './containers/TrackOrder';
+
+import './index.css';
+
+ReactDOM.render(
+  <Router history={browserHistory}>
+    <Route path="/" component={Home} />
+    <Route path="/order/:id" component={TrackOrder} />
+  </Router>,
+  document.getElementById('root')
+);
